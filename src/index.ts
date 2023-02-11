6a06a66dcc06b3f905e95e09fd1254efd428bb77
Felipe Barcelos <felipebarcelospro@iMac-de-Felipe.local>
2023-02-11 11:02:47 -0300

    feat: add event error handling

--- src/index.ts
+++ src/index.ts
@@ -10,23 +10,38 @@ export class NotifyLog {
   async publish(event: {
     name: string;
     channel: string;
-    notify: boolean;
-    icon?: string;
+    notify: boolean;    
     description?: string;
+    icon?: string;
     tags?: { [key: string]: string | number };
     message?: string;
-  }) {
-    // configure headers
+  }): Promise<{
+    error: any;
+    ok: boolean;
+  }> {
     const headers = {
       "Content-Type": "application/json",
       authorization: this.apiKey,
     };
 
-    // make the request to NotifyLog
-    await axios.post(
-      "https://app.notifylog.com/api/v1/event",
-      event,
-      { headers: headers }
-    );
+    try {
+      await axios.post(
+        "https://app.notifylog.com/api/v1/event",
+        event,
+        { headers: headers }
+      );
+
+      return {
+        ok: true,
+        error: null,
+      }
+    } catch (error) {
+      console.error(error);
+
+      return {
+        ok: false,
+        error: error,
+      }
+    }
   }
 }
